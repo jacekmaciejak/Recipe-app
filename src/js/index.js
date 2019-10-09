6f0d057e7597b02d754a357d09e50bbabcb72419
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-10-09 19:46:57 +0200

    Search model added

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,5 @@
-//70e825316658a43a8311c70321ebc460
-//Search API URL  https://www.food2fork.com/api/search
-//Recipe details API URL   https://www.food2fork.com/api/get
-
-import axios from "axios";
-// 1
-async function getResault(query) {
-  const proxy = "https://cors-anywhere.herokuapp.com/"; //obejscie do strony z API
-  const key = "70e825316658a43a8311c70321ebc460"; //klucz API ze strony
-
-  //try - catch, wyswietli blad jesli jest zle zapytanie
-  try {
-    const res = await axios(
-      `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`
-    ); //zamiast metody fetch, dziala na wszystkich przegladarkach, musimy ja zainstalowac, sluzy do pobierania danych, jako parametr dajemy adres API URL, oddzielamy "?" i dodajemy kolejne parametry
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResault("pizza");
+import Search from "./models/Search";
+// ----------- 3 ----------------
+const search = new Search("pizza");
+console.log(search);
+search.getResault();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,26 @@
-// export default "jdadwkjfnf fnwskfnsf jnslfjnsf";
+//70e825316658a43a8311c70321ebc460
+//Search API URL  https://www.food2fork.com/api/search
+//Recipe details API URL   https://www.food2fork.com/api/get
+import axios from "axios";
+
+//-------------- 2 ----------------
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  //------------- 1 -------------------
+  async getResault() {
+    const proxy = "https://cors-anywhere.herokuapp.com/"; //obejscie do strony z API
+    const key = "70e825316658a43a8311c70321ebc460"; //klucz API ze strony
+    //try - catch, wyswietli blad jesli jest zle zapytanie
+    try {
+      const res = await axios(
+        `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      ); //zamiast metody fetch, dziala na wszystkich przegladarkach, musimy ja zainstalowac, sluzy do pobierania danych, jako parametr dajemy adres API URL, oddzielamy "?" i dodajemy kolejne parametry
+      this.result = res.data.recipes;
+      //console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
