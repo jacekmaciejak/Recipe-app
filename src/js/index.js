2c031f58bd4baf2c174d6d33f95d82acf70c836a
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-10-09 19:31:34 +0200

    Try and Catch added

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,3 @@
-// import string from "./models/Search";
-// //import { add as a, multiplay, ID } from "./views/searchView";
-// import * as searchView from "./views/searchView";
-// console.log(
-//   `using importeddddd funcions! ${searchView.add(
-//     searchView.ID,
-//     5
-//   )} => ${searchView.multiplay(4, 10)} => ${searchView.multiplay(4, 9)} `
-// );
-
 //70e825316658a43a8311c70321ebc460
 //Search API URL  https://www.food2fork.com/api/search
 //Recipe details API URL   https://www.food2fork.com/api/get
@@ -17,10 +7,16 @@ import axios from "axios";
 async function getResault(query) {
   const proxy = "https://cors-anywhere.herokuapp.com/"; //obejscie do strony z API
   const key = "70e825316658a43a8311c70321ebc460"; //klucz API ze strony
-  const res = await axios(
-    `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`
-  ); //zamiast metody fetch, dziala na wszystkich przegladarkach, musimy ja zainstalowac, sluzy do pobierania danych, jako parametr dajemy adres API URL, oddzielamy "?" i dodajemy kolejne parametry
-  const recipes = res.data.recipes;
-  console.log(recipes);
+
+  //try - catch, wyswietli blad jesli jest zle zapytanie
+  try {
+    const res = await axios(
+      `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`
+    ); //zamiast metody fetch, dziala na wszystkich przegladarkach, musimy ja zainstalowac, sluzy do pobierania danych, jako parametr dajemy adres API URL, oddzielamy "?" i dodajemy kolejne parametry
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
 }
 getResault("pizza");
