2c55b0567f35b3770e07ae980eaf9a28869a955b
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-10-10 13:08:35 +0200

    Search view functions added - part 1

---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+//---------- 5 ---------------
+//tworzymy komponenty z pobranymi elementami HTML
+export const elements = {
+  searchForm: document.querySelector(".search"),
+  searchInput: document.querySelector(".search__field"),
+  searchResList: document.querySelector(".results__list")
+};
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,35 @@
-// export const add = (a, b) => a + b;
-// export const multiplay = (a, b) => a * b;
-// export const ID = 23;
+import { elements } from "./base";
+
+//------------------- 5 -------------
+export const getInput = () => elements.searchInput.value; //pobiera wartosc z pola search
+
+export const clearInput = () => {
+  elements.searchInput.value = ""; //czyszczenie pola wyszukujacego
+};
+export const clearResList = () => {
+  elements.searchResList.innerHTML = ""; //czyszczenie pola z wynikami wyszukiwania
+};
+
+//funkcja wyswietlajaca wynik wyszukiwania
+const renderRecipe = recipe => {
+  //dynamiczne pole z wynikow wyszukiwania
+  const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${recipe.title}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
+  //wyswietlanie zmiennej markaup w elemencie searchResList pobranym z base.js
+  elements.searchResList.insertAdjacentHTML("beforeend", markup);
+};
+//funkcja wykonujaca petle po funkcji powyzej
+export const renderResults = recipes => {
+  recipes.forEach(renderRecipe);
+};
