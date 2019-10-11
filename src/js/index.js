1ce379734e7517794b956146f494d0f9a115e34d
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-10-11 20:06:21 +0200

    Recipe view functions added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
+import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /**Global state of the app
@@ -60,7 +61,8 @@ const controlRecipe = async () => {
 
   if (id) {
     //Prepare UI for changes
-
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
     //Create new recipe object
     state.recipe = new Recipe(id); //nowy obiekt
 
@@ -72,6 +74,8 @@ const controlRecipe = async () => {
       state.recipe.calcTime();
       state.recipe.calcServings();
       //Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (err) {
       alert("Error processing recipe!");
     }
