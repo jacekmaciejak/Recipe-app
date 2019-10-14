9e46924db82247facbcbfab5ebd159c11ecc965c
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-10-14 17:24:00 +0200

    Shopping List View added

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -6,13 +6,25 @@ export default class List {
     }
 
     //Metoda dodajaca nowy item do listy
-    addItem(id, count, unit, ingredient) {
+    addItem(count, unit, ingredient) {
         const item = {
             id: uniqid(),
             count,
             unit,
             ingredient
         }
+        this.items.push(item);
+        return item;
     }
 
+    //usuwanie itema z tablicy items
+    deleteItem(id) {
+        const index = this.items.findIndex(el => el.id === id);
+        //[2,4,8] splice(1,2) --> returns [4,8] original array is [2]
+        //[2,4,8] slice(1,2) --> returns 4, original array is [2,4,8]
+        this.items.splice(index, 1)
+    }
+    updateCount(id, newCount) {
+        this.items.find(el => el.id === id).count = newCount;
+    }
 }
