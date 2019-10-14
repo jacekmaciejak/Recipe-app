a0a74a81a7dc7cddbb43720f6f461b063e3a4c45
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-10-14 21:19:33 +0200

    Persistent data with localStorage added

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -6,6 +6,9 @@ export default class Likes {
   addLike(id, title, author, img) {
     const like = { id, title, author, img };
     this.likes.push(like);
+
+    //Perist data in localStorage
+    this.persistData();
     return like;
   }
   deleteLike(id) {
@@ -13,11 +16,23 @@ export default class Likes {
     //[2,4,8] splice(1,2) --> returns [4,8] original array is [2]
     //[2,4,8] slice(1,2) --> returns 4, original array is [2,4,8]
     this.likes.splice(index, 1);
+
+    //Perist data in localStorage
+    this.persistData();
   }
   isLiked(id) {
     return this.likes.findIndex(el => el.id === id) !== -1;
   }
   getNumLikes() {
     return this.likes.length;
+  }
+  persistData() {
+    localStorage.setItem("likes", JSON.stringify(this.likes));
+  }
+  readStorage() {
+    const storage = JSON.parse(localStorage.getItem("likes"));
+
+    //Restoring likes from the localStorage
+    if (storage) this.likes = storage;
   }
 }
